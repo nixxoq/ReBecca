8451f337b8f917fd70b6af401373c64c914307ea
nixxoq <nixxoq@outlook.com>
2026-09-03 17:42:58 +0300

    add reg_location value for RegKey

--- src/model.rs
+++ src/model.rs
@@ -30,6 +30,29 @@ pub struct RegKey {
     pub subkeys: Vec<RegKey>,
     pub values: Vec<RegValue>,
     pub class_name: Option<String>,
+    pub location: Option<RegKeyLoc>,
+}
+
+impl RegKey {
+    pub fn cell_offset(&self) -> Option<usize> {
+        self.location.as_ref().map(|l| l.cell_offset)
+    }
+
+    pub fn location(&self) -> Option<&RegKeyLoc> {
+        self.location.as_ref()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct RegKeyLoc {
+    pub cell_offset: usize,   // Absolute file offset in raw_data
+    pub cell_size: usize,     // Total allocated cell size
+    pub parent_offset: u32,   // Relative cell offset of parent NK (or MAX_OFFSET)
+    pub subkeys_offset: u32,  // Relative cell offset of subkey index (or MAX_OFFSET)
+    pub values_offset: u32,   // Relative cell offset of value list (or MAX_OFFSET)
+    pub security_offset: u32, // Relative cell offset of security descriptor (or MAX_OFFSET)
+    pub class_offset: u32,    // Relative cell offset of class name (or MAX_OFFSET)
+    pub flags: u16,           // NK flags (KEY_COMP_NAME, etc.)
 }
 
 #[derive(Debug, Clone)]
@@ -37,7 +60,7 @@ pub struct RegValue {
     pub name: String,
     pub value_type: RegValueType,
     pub data: Vec<u8>,
-    pub(crate) location: Option<RegValueLoc>
+    pub(crate) location: Option<RegValueLoc>,
 }
 
 #[derive(Debug, Clone)]
@@ -45,7 +68,7 @@ pub struct RegValueLoc {
     pub(crate) cell_offset: usize,
     pub(crate) data_offset: usize,
     pub(crate) data_size: u32,
-    pub(crate) inline: bool
+    pub(crate) inline: bool,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -64,6 +87,22 @@ pub enum RegValueType {
 }
 
 impl RegValueType {
+    pub fn to_u32(&self) -> u32 {
+        match self {
+            RegValueType::None => 0,
+            RegValueType::String => 1,
+            RegValueType::ExpandString => 2,
+            RegValueType::Binary => 3,
+            RegValueType::Dword => 4,
+            RegValueType::DwordBigEndian => 5,
+            RegValueType::Link => 6,
+            RegValueType::MultiString => 7,
+            RegValueType::ResourceList => 8,
+            RegValueType::Qword => 11,
+            RegValueType::Unknown(value) => *value,
+        }
+    }
+
     pub fn from(value: u32) -> Self {
         match value {
             0 => RegValueType::None,
